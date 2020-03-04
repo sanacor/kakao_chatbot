8f52d10cf916261bf1efefe8c698f596c27ac461
강산하 <ncsana@gmail.com>
2020-03-04 23:46:32 +0900

    fallback block 위치 변경

--- chatbot/cashier_chatbot/fallback_block/fallback.py
+++ chatbot/cashier_chatbot/fallback_block/fallback.py
@@ -7,8 +7,11 @@ class Fallback:
         pass
 
     def default_response(self):
-        with open('./fallback_content.json') as json_file:
+        # with open('./fallback_content.json') as json_file:
+        with open('./chatbot/cashier_chatbot/fallback_block/fallback_content.json') as json_file:
             fallback_response = json.load(json_file)
+            print('fallback_response')
+            print(fallback_response)
             return fallback_response
 
 
