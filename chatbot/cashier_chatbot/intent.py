cb76a114b19cb083eaf73b58499cfdbb277e7777
강산하 <ncsana@gmail.com>
2020-03-22 21:32:38 +0900

    인텐트 매칭 추가

--- chatbot/cashier_chatbot/intent.py
+++ chatbot/cashier_chatbot/intent.py
@@ -1,37 +1,48 @@
 from chatbot.cashier_chatbot.google_spreadsheet import GoogleSpreadSheet
 from chatbot.cashier_chatbot.fallback_block.fallback import Fallback
+from chatbot.cashier_chatbot.welcome_block.welcome import Welcome
+
 gss = GoogleSpreadSheet()
 fallback = Fallback()
+welcome = Welcome()
 
 
 class Intent:
+    WELCOME = '웰컴 블록'
+    FALL_BACK = '폴백 블록'
+
     NEW_PS4_GAME_LIST = '신규 PS4 게임 리스트'
     USED_PS4_GAME_LIST = '중고 PS4 게임 리스트'
     PRICE_OF_NEW_PS4_GAME = '신규 PS4 게임 가격 문의'
     PRICE_OF_USED_PS4_GAME = '중고 PS4 게임 가격 문의'
-    FALL_BACK = '폴백 블록'
+
+
 
 
 intent_to_action = {
-    Intent.NEW_PS4_GAME_LIST: gss.get_used_title,
-    Intent.USED_PS4_GAME_LIST: gss.get_used_title,
-    Intent.PRICE_OF_NEW_PS4_GAME: gss.get_used_title,
-    Intent.PRICE_OF_USED_PS4_GAME: gss.get_used_title,
-    Intent.FALL_BACK: fallback.default_response
+    Intent.WELCOME: welcome.default_response,
+    Intent.FALL_BACK: fallback.default_response,
+
+    Intent.NEW_PS4_GAME_LIST: gss.get_new_release_game_list,
+    Intent.USED_PS4_GAME_LIST: gss.get_used_game_list,
+    Intent.PRICE_OF_NEW_PS4_GAME: gss.get_new_release_game_price,
+    Intent.PRICE_OF_USED_PS4_GAME: gss.get_used_game_price
+
 }
 
-block_to_block_id = {
-    Intent.NEW_PS4_GAME_LIST: '5e5f9e8c15e0d80001bd7f90',
-    Intent.USED_PS4_GAME_LIST: '5e5f9e99c209c10001dcfd24',
-    Intent.PRICE_OF_NEW_PS4_GAME: '5e5f9ea863420a0001e3691d',
-    Intent.PRICE_OF_USED_PS4_GAME: '5e5f9eb13ad5250001d682b2'
-}
+# block_to_block_id = {
+#     Intent.NEW_PS4_GAME_LIST: '5e5f9e8c15e0d80001bd7f90',
+#     Intent.USED_PS4_GAME_LIST: '5e5f9e99c209c10001dcfd24',
+#     Intent.PRICE_OF_NEW_PS4_GAME: '5e5f9ea863420a0001e3691d',
+#     Intent.PRICE_OF_USED_PS4_GAME: '5e5f9eb13ad5250001d682b2'
+# }
 
 
 class Matcher:
 
-    def __init__(self, intent):
+    def __init__(self, intent, request):
         self.intent = intent
+        self.request = request
 
     def execute(self):
         try:
@@ -39,10 +50,13 @@ class Matcher:
                 print('No matching intent'+self.intent)
                 return
             action = intent_to_action[self.intent]
+            # fallback_response = fallback.default_response()
+            # quick_replies = fallback_response['template']['quickReplies']
+            print('Before action')
+            result_response = action(self.request)
+
             fallback_response = fallback.default_response()
             quick_replies = fallback_response['template']['quickReplies']
-            print('Before action')
-            result_response = action()
             result_response['template'].update({'quickReplies': quick_replies})
             return result_response
         except KeyError:
