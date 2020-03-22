113c9843e887ac1d958ab41101c64908fbbb42a0
강산하 <ncsana@gmail.com>
2020-03-22 21:34:21 +0900

    no message

--- chatbot/cashier_chatbot/fallback_block/fallback.py
+++ chatbot/cashier_chatbot/fallback_block/fallback.py
@@ -6,7 +6,7 @@ class Fallback:
     def __init__(self):
         pass
 
-    def default_response(self):
+    def default_response(self, request=None):
         # with open('./fallback_content.json') as json_file:
         with open('./chatbot/cashier_chatbot/fallback_block/fallback_content.json') as json_file:
             fallback_response = json.load(json_file)
